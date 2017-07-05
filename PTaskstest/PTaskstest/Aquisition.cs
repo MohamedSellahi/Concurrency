a8cc0b3c0a64c62385f38e83d23ae10d1778e4c1
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-07-05 16:42:22 +0200

    PTask, tst0

---
+++ PTaskstest/PTaskstest/Aquisition.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PTaskstest {
+   class Aquisition {
+      public Queue<int> data = null;
+      public int _queueSize = 1000;
+      public bool IsQueFull { get; set; }
+      private int maxIteration;
+      private object myLock = new object();
+      // 
+
+
+      public Aquisition() {
+         data = new Queue<int>(_queueSize);
+         IsQueFull = false;
+         maxIteration = 50;
+      }
+
+
+      public void StartAquisition() {
+         Task t = new Task(() => {
+            int num = 0;
+            while (maxIteration-- > 0) {
+               lock (myLock) {
+                  data.Enqueue(num++);
+               }
+               Thread.Sleep(300);
+            }
+            IsQueFull = true;
+         });
+         // start task;
+         t.Start();
+      }
+
+      public void getDataAndEmpty() {
+         // 
+         lock (myLock) {
+
+            var arr = this.data.ToArray();
+            foreach (var i in arr) {
+               Console.Write("{0}, ", i);
+            }
+            // empty buffer
+            data.Clear();
+         }
+         Console.WriteLine();
+         Thread.Sleep(2000);
+      }
+   }
+}
---
+++ PTaskstest/PTaskstest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTaskstest {
+   class Program {
+      static void Main(string[] args) {
+         Aquisition aq = new Aquisition();
+
+         // start aquisition 
+         aq.StartAquisition();
+
+         Console.WriteLine("Value found in the buffer");
+         while (!aq.IsQueFull) {
+            aq.getDataAndEmpty();
+         }
+      }
+   }
+}
